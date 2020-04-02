8200eea03e21bef56080a966a86ded6e0fd7d5a2
Alexander Evgin <aleasims@gmail.com>
2020-04-02 22:49:51 +0300

    Breathe life into Slavka

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-# Source: https://medium.com/python4you/creating-telegram-bot-and-deploying-it-on-heroku-471de1d96554
 import logging
 import os
 import random
@@ -7,15 +6,16 @@ import sys
 from telegram.ext import Updater, CommandHandler
 
 LOG_MSG = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-logging.basicConfig(level=logging.INFO,
-                    format=LOG_MSG)
+logging.basicConfig(level=logging.INFO, format=LOG_MSG)
 logger = logging.getLogger()
 
 mode = os.getenv("MODE")
 TOKEN = os.getenv("TOKEN")
+
 if mode == "dev":
     def run(updater):
         updater.start_polling()
+
 elif mode == "prod":
     def run(updater):
         PORT = int(os.environ.get("PORT", "8443"))
@@ -25,6 +25,7 @@ elif mode == "prod":
                               url_path=TOKEN)
         updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(
             HEROKU_APP_NAME, TOKEN))
+
 else:
     logger.error("No MODE specified!")
     sys.exit(1)
@@ -32,15 +33,61 @@ else:
 
 def start_handler(bot, update):
     logger.info("User {} started bot".format(update.effective_user["id"]))
-    update.message.reply_text(
-        "Hello from Python!\nPress /random to get random number")
+    update.message.reply_text("привет, пёс, это Славка!")
 
 
-def random_handler(bot, update):
-    number = random.randint(0, 10)
-    logger.info("User {} randomed number {}".format(
-        update.effective_user["id"], number))
-    update.message.reply_text("Random number: {}".format(number))
+SLAVKA_PHRASES = [
+    'версута сын собаки!',
+    'ору с гей-мерзкого чата)',
+    'фу блять мерзкий войсчат',
+    'какая нахуй телега, мир сошел с ума...',
+    'у меня эта помойка работает еле-еле потому что в России блять запрещена',
+    'ПО ФОРМУЛЕ!!!',
+    'предлагаю присесть в коридоре',
+    'МАРАТ ДА ПО ФОРМУЛЕ!',
+    'Гребной коронавирус...',
+    'ты как рубашку снял, Петров?',
+    'Марти одевает подштанники...',
+    'почему Степан генерирует рандомный вопрос, начинающийся со слова "почему", скорее всего не имеющий разумного ответа, к тому же с изначально сомнительным утверждением в теле вопроса?',
+    'ЗДЕСЬ ЛЮДЕЙ ЕБУТ',
+    'челы, вы...',
+    'АААААААААААААААААААААААААААА',
+    'может тебе неудобства доставить',
+    'Сань ты что по этому поводу скажешь?',
+    'ровно 2,573 человека',
+    'ля, почему вы форсите столько хуйни с двача? 90 процентов контента, который вы оттуда скидываете, несмешно и кринжово',
+    'общего чата не заслуживает',
+    'то чувство, когда у Узумаки Наруто больше посещений философии, чем у меня...',
+    'комната умирает',
+    'чел, ты...',
+    'ляг поспи...',
+    'дип фэйк закрыли',
+    'rest in RIP Степан...',
+    'никово...',
+    'Челы, вы... Где нахуй?',
+    'Почему не в палате?',
+    'Хули в комнате пусто :(',
+    "oh u're approaching me?",
+    'ещё сука молчит в ВК не отвечает на крысу...',
+    'вовремя я заболел... впрочем, будь я на месте, наверн ничего бы не изменилось?',
+    'красава Марти',
+    'найс режим',
+    'я болею(',
+    'Чел а если серьезно?',
+    'Его идеи... полная хуйня',
+    'чел... ты ебанулся?',
+    'уберемся на скорую руку',
+    'ладно, я подъеду, челы...',
+    'УРА)'
+]
+
+
+def speak_handler(bot, update):
+    id_ = update.effective_user["id"]
+    number = random.randint(0, len(SLAVKA_PHRASES))
+    phrase = SLAVKA_PHRASES[number]
+    logger.info("User {} asked for speak: {}".format(id_, phrase))
+    update.message.reply_text(phrase)
 
 
 if __name__ == '__main__':
@@ -48,6 +95,6 @@ if __name__ == '__main__':
     updater = Updater(TOKEN)
 
     updater.dispatcher.add_handler(CommandHandler("start", start_handler))
-    updater.dispatcher.add_handler(CommandHandler("random", random_handler))
+    updater.dispatcher.add_handler(CommandHandler("speak", speak_handler))
 
     run(updater)
