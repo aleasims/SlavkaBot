10c3b422c0bd877a515f3554a6b177bd28a8d614
Alexander Evgin <aleasims@gmail.com>
2020-04-02 21:53:07 +0300

    Initial bot

---
+++ bot.py
@@ -0,0 +1,53 @@
+# Source: https://medium.com/python4you/creating-telegram-bot-and-deploying-it-on-heroku-471de1d96554
+import logging
+import os
+import random
+import sys
+
+from telegram.ext import Updater, CommandHandler
+
+LOG_MSG = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+logging.basicConfig(level=logging.INFO,
+                    format=LOG_MSG)
+logger = logging.getLogger()
+
+mode = os.getenv("MODE")
+TOKEN = os.getenv("TOKEN")
+if mode == "dev":
+    def run(updater):
+        updater.start_polling()
+elif mode == "prod":
+    def run(updater):
+        PORT = int(os.environ.get("PORT", "8443"))
+        HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
+        updater.start_webhook(listen="0.0.0.0",
+                              port=PORT,
+                              url_path=TOKEN)
+        updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(
+            HEROKU_APP_NAME, TOKEN))
+else:
+    logger.error("No MODE specified!")
+    sys.exit(1)
+
+
+def start_handler(bot, update):
+    logger.info("User {} started bot".format(update.effective_user["id"]))
+    update.message.reply_text(
+        "Hello from Python!\nPress /random to get random number")
+
+
+def random_handler(bot, update):
+    number = random.randint(0, 10)
+    logger.info("User {} randomed number {}".format(
+        update.effective_user["id"], number))
+    update.message.reply_text("Random number: {}".format(number))
+
+
+if __name__ == '__main__':
+    logger.info("Starting bot")
+    updater = Updater(TOKEN)
+
+    updater.dispatcher.add_handler(CommandHandler("start", start_handler))
+    updater.dispatcher.add_handler(CommandHandler("random", random_handler))
+
+    run(updater)
