798fc2148624ec9553ad28035c082b09b6c980a2
Alexander Evgin <aleasims@gmail.com>
2020-04-04 03:00:20 +0300

    Fix File not found error

--- slavkabot/slavka.py
+++ slavkabot/slavka.py
@@ -2,7 +2,7 @@ import random
 
 
 class Slavka:
-    def __init__(self, phrases='phrases.txt'):
+    def __init__(self, phrases='slavkabot/phrases.txt'):
         with open(phrases, 'r', encoding='utf-8') as f:
             self.phrases = [phrase.strip() for phrase in f.readlines()]
 
