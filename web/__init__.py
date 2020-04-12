9d7c799c20ea0c8989344e52cf0f35b6715fcc91
Alexander Evgin <aleasims@gmail.com>
2020-04-12 06:04:48 +0300

    Fix naming error

--- web/__init__.py
+++ web/__init__.py
@@ -1,18 +1,17 @@
-from flask import Flask, request
+from flask import Flask
 
 from slavkabot.slavka import Slavka
 
 greeting = Slavka().greeting()
 
-web = Flask(__name__)
+app = Flask(__name__)
 
 
-@web.route('/')
+@app.route('/')
 def home():
     return f'<h1>{greeting}</h1>'
 
 
-@web.route('/<token>', methods=['POST'])
+@app.route('/<token>', methods=['POST'])
 def telegram_ping(token):
-    web.logger().info(f'Request from {request.remote_addr}')
     return ''
