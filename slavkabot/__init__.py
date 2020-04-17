d9900090a1eeba4d179dd8f66a1738e3d78649df
Alexander Evgin <aleasims@gmail.com>
2020-04-17 22:40:45 +0300

    Rename entypoint module

--- slavkabot/__init__.py
+++
@@ -1,9 +0,0 @@
-from slavkabot.config import build_config, download_model
-from slavkabot.bot import Bot
-
-
-def main():
-    config = build_config()
-    download_model()
-    bot = Bot(config)
-    bot.start()
