5b427b1392b426144c0289af027f36bc0dc758df
Alexander Evgin <aleasims@gmail.com>
2020-04-24 14:46:17 +0300

    Add config file to CLI arguments for main

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from slavkabot.config import build_config
+import sys
+from argparse import ArgumentParser
+
 from slavkabot.bot import Bot
+from slavkabot.config import ConfigurationError, build_config
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('-c', '--conf', default='config.yml',
+                        help='Path to configuration.yaml file')
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    config = build_config('config.yml')
+    args = parse_args()
+    try:
+        config = build_config(args.conf)
+    except ConfigurationError as e:
+        sys.stderr.write(f'Configuration error: {e}')
+        sys.exit(1)
+
     bot = Bot(config)
     bot.start()
